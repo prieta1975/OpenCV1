e9ea7975d5d10d0c6a5721e296d6519d6ba36ca0
prieta1975 <antonio_prieto_jimenez@hotmail.com>
2015-12-13 20:10:15 +0100

    Paquetes instalados usando pip (numpy, opencv) Corregidas rutas del fichero de ejemplo Funcionando corrrectamente

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Python with OPenCV Project 1
 # Image
 
+import numpy
 import cv2
 
 # Read an image
-img cv2.imread('C:\Users\Admin\Desktop\P1070543.JPG',0)
+img = cv2.imread('C:\\Users\\Admin\\Desktop\\P1070543.JPG',0)
 
 # Show image in a window
 cv2.imshow('Ventana de imagen',img)
@@ -14,5 +15,5 @@ k = cv2.waitKey(0)
 if k == ord('e'):
     cv2.destroyAllWindows()
 elif k== ord('s'):
-    cv2.imwrite('C:\Users\Admin\Desktop\P1070543_B.JPG',img)
+    cv2.imwrite('C:\\Users\\Admin\\Desktop\\P1070543_B.JPG',img)
     cv2.destroyAll_Windows()
