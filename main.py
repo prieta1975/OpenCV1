57516f1a9ec35988a52b36c112509026a306f2dd
prieta1975 <antonio_prieto_jimenez@hotmail.com>
2015-12-13 12:13:25 +0100

    Primera versión, todavía sin tener opencv2 importado

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Prueba
+# Python with OPenCV Project 1
+# Image
+
+import cv2
+
+# Read an image
+img cv2.imread('C:\Users\Admin\Desktop\P1070543.JPG',0)
+
+# Show image in a window
+cv2.imshow('Ventana de imagen',img)
+
+k = cv2.waitKey(0)
+
+if k == ord('e'):
+    cv2.destroyAllWindows()
+elif k== ord('s'):
+    cv2.imwrite('C:\Users\Admin\Desktop\P1070543_B.JPG',img)
+    cv2.destroyAll_Windows()
